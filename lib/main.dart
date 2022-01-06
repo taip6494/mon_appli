2c01962e951689bfcadd873bba81e8b6033bb20a
taip6494 <palisseauthomas@gmail.com>
2022-01-06 15:12:18 +0400

    V2.0 Modification de la police , ajout de divider et icon

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class BoxScreen extends StatelessWidget {
     double deviceHeight = MediaQuery.of(context).size.height;
 
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Container(
           height: deviceHeight * 0.1,
@@ -29,9 +30,8 @@ class BoxScreen extends StatelessWidget {
           ),
         ),
         Container(
-          height: deviceHeight * 0.25,
-          width: deviceWidth * 1,
-          color: Colors.orange,
+          height: deviceHeight * 0.15,
+          margin: EdgeInsets.fromLTRB(12, 150, 12, 12),
           alignment: Alignment.center,
           child: CircleAvatar(
             radius: 70,
@@ -41,40 +41,39 @@ class BoxScreen extends StatelessWidget {
                   "https://scontent-cdt1-1.xx.fbcdn.net/v/t39.30808-6/262639120_4822422531143309_8778383907595570379_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=QTXnyBxsF6YAX9PsjX8&tn=KPwPIshsNhD9QjmJ&_nc_ht=scontent-cdt1-1.xx&oh=00_AT_6tlz7usL6qCS3mFvaFNv9SBHzQRiKgUsndkqbe6DqQg&oe=61DA93D7"),
             ),
           ),
+          width: deviceWidth * 1,
+          //color: Colors.white,
           //Put your child widget here.
         ),
         Container(
-          height: deviceHeight * 0.15,
+          height: deviceHeight * 0.05,
           width: deviceWidth * 0.8,
-          color: Colors.orange,
+          //color: Colors.white,
+          //margin: EdgeInsets.fromLTRB(12,0, 0, 12),
           alignment: Alignment.center,
           child: Center(
             child: Text(
-              "Je me présente Thomas Palisseau.Je suis développeur débutant en web et mobile. ",
+              "Thomas Palisseau",
               style: TextStyle(
-                fontSize: 15.0,
-                height: 02.0,
+                fontSize: 28.0,
+                fontFamily: 'Pacifico',
                 color: Colors.white,
-                fontWeight: FontWeight.bold,
+                //fontWeight: FontWeight.bold,
               ),
             ),
           ),
         ),
         Container(
-          height: deviceHeight * 0.05,
-          width: deviceWidth * 0.8,
-          color: Colors.white,
+          height: deviceHeight * 0.01,
+          width: deviceWidth * 0.40,
+          margin: EdgeInsets.fromLTRB(12, 12, 12, 12),
           alignment: Alignment.center,
-          child: Center(
-            child: Text(
-              "GitHub: Taip6494 ",
-              style: TextStyle(
-                fontSize: 15.0,
-                height: 02.0,
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+          //color: Colors.red,
+          child: Divider(
+            thickness: 2,
+            indent: 5,
+            endIndent: 0,
+            color: Colors.white,
           ),
         ),
         Container(
@@ -83,17 +82,25 @@ class BoxScreen extends StatelessWidget {
           margin: EdgeInsets.fromLTRB(12, 12, 12, 12),
           color: Colors.white,
           alignment: Alignment.center,
-          child: Center(
-            child: Text(
-              "Email: palisseau@protonmail.com ",
+          child: Row(children: [
+            SizedBox(
+              width: 80,
+              child: Icon(
+                Icons.email,
+                color: Colors.orange,
+                size: 24.0,
+              ),
+            ),
+            Text(
+              "palisseau@protonmail.com ",
+              textAlign: TextAlign.center,
               style: TextStyle(
-                fontSize: 15.0,
-                height: 02.0,
+                fontSize: 16.0,
                 color: Colors.black,
                 fontWeight: FontWeight.bold,
               ),
             ),
-          ),
+          ]),
         ),
         Container(
           height: deviceHeight * 0.05,
@@ -101,16 +108,27 @@ class BoxScreen extends StatelessWidget {
           margin: EdgeInsets.fromLTRB(12, 0, 12, 12),
           color: Colors.white,
           alignment: Alignment.center,
-          child: Center(
-            child: Text(
-              "Téléphone: 0693 33 26 83 ",
-              style: TextStyle(
-                fontSize: 15.0,
-                height: 02.0,
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
+          child: Row(
+            children: [
+              SizedBox(
+                width: 80,
+                child: Icon(
+                  Icons.smartphone,
+                  color: Colors.orange,
+                  size: 24.0,
+                ),
               ),
-            ),
+              const SizedBox(height: 200),
+              Text(
+                "0693 33 26 83 ",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
           ),
         ),
       ],
